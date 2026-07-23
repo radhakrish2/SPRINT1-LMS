ac13b96656997169e13f13cf70b412e68bda421a
RK1 <radhakrish2@gmail.com>
2026-07-23 22:29:32 +0530

    3a

--- src/main/java/com/learnhub/security/SecurityConfig.java
+++ src/main/java/com/learnhub/security/SecurityConfig.java
@@ -22,6 +22,7 @@ public class SecurityConfig {
 
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
+    
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http)
             throws Exception {
